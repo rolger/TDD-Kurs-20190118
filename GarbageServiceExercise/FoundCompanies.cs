95a0eee9c4e1ac968ad9665f38294a8199d3ffe5
Roland.Germ <roland.germ@anecon.com>
2019-04-26 00:10:14 +0200

    initial commit

---
+++ GarbageServiceExercise/FoundCompanies.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace GarbageServiceExercise
+{
+    public class FoundCompanies
+    {
+    }
+}
---
+++ GarbageServiceExercise/SearchEngine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace GarbageServiceExercise
+{
+    public class Company
+    {
+        public Company(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public List<string> Garbage { get; set; }
+    }
+
+    public interface ICompanyRepository
+    {
+        List<Company> FindBy(string keyword);
+        void SaveNewCompany(string name, List<string> garbage);
+    }
+
+    public class SearchEngine
+    {
+        private readonly ICompanyRepository repo;
+        private readonly GarbageParser parser;
+
+        public FoundCompanies Search(string input)
+        {
+            return null;
+        }
+    }
+}

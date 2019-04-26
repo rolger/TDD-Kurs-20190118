e1207a1307bcbb6e59d48174e77906b2bb19abbf
Roland.Germ <roland.germ@nagarro.com>
2019-04-26 13:04:46 +0200

    moved implementation to new class

---
+++ ROT-13/Encoder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ROT_13
+{
+    public class Encoder
+    {
+        public string Rotate(string input)
+        {
+            var transformedInput = input.ToUpper();
+            transformedInput = transformedInput.Replace("Ö", "OE");
+            transformedInput = transformedInput.Replace("Ä", "AE");
+            transformedInput = transformedInput.Replace("Ü", "UE");
+            transformedInput = transformedInput.Replace("ß", "SS");
+            var result = "";
+            foreach (char c in transformedInput)
+            {
+                result += RotateCharacter(c).ToString();
+            }
+            return result;
+        }
+
+        private static char RotateCharacter(char c)
+        {
+            if (Char.IsLetter(c))
+            {
+                return RotateLetter(c);
+            }
+
+            return c;
+        }
+
+        private static char RotateLetter(char character)
+        {
+            var currentAsciValue = character + 13;
+            if (currentAsciValue > 'Z')
+            {
+                currentAsciValue -= 26;
+            }
+            return (char)currentAsciValue;
+        }
+    }
+
+}
+}
--- ROT-13/EncoderTest.cs
+++ ROT-13/EncoderTest.cs
@@ -55,40 +55,10 @@ namespace ROT_13
             result.Should().Be("URYYB, JBEYQ");
         }
 
-
         private string Rotate(string input)
         {
-            var transformedInput = input.ToUpper();
-            transformedInput = transformedInput.Replace("Ö", "OE");
-            transformedInput = transformedInput.Replace("Ä", "AE");
-            transformedInput = transformedInput.Replace("Ü", "UE");
-            transformedInput = transformedInput.Replace("ß", "SS");
-            var result = "";
-            foreach (char c in transformedInput)
-            {
-                result += RotateCharacter(c).ToString();
-            }
-            return result;
+            new Encoder().Rotate(input);
         }
 
-        private static char RotateCharacter(char c)
-        {
-            if (Char.IsLetter(c))
-            {
-                return RotateLetter(c);
-            }
-
-            return c;
-        }
-
-        private static char RotateLetter(char character)
-        {
-            var currentAsciValue = character + 13;
-            if (currentAsciValue > 'Z')
-            {
-                currentAsciValue -= 26;
-            }
-            return (char)currentAsciValue;
-        }
     }
 }
